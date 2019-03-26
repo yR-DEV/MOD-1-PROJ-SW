4368014cf6a676d4fbcbac25a8eff0aab75a53a8
yeah-right <claar.zack@gmail.com>
2019-03-26 14:51:56 -0600

    moved some stuff around in schema, added method in run.rb to get shows from the DB even though the shows table isn't being populated. Need to figure that out next

--- bin/run.rb
+++ bin/run.rb
@@ -1,14 +1,5 @@
 require_relative '../config/environment'
 
-
-
-#puts "HELLO WORLD"
-# def puts_shows(shows)
-#   shows.each do |show|
-#     puts show
-#   end
-# end
-
 def get_shows
   puts Artist.get_artists
 end
@@ -17,10 +8,16 @@ def get_venues
   puts Venue.get_venues
 end
 
+def get_shows
+  puts Show.get_shows
+end
+
 puts "Enter artists to see upcoming shows they are in:"
 response = gets.chomp
-  if response == "artists"
-    get_shows()
-  elsif response == "venues"
-    get_venues()
-  end
+if response == "artists"
+  get_shows()
+elsif response == "venues"
+  get_venues()
+elsif response == "shows"
+  get_shows()
+end
--- lib/models/show.rb
+++ lib/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
-belongs_to :artist
-belongs_to :venue 
+  belongs_to :artist
+  belongs_to :venue
+
+  def self.get_shows
+    sql = <<-SQL
+      SELECT * FROM shows
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
+  end
 end
