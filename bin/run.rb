e85904bdc19fddcef2eafe37599a3b5d7bc4c902
sarahsakordaniels <ssdaniels13@gmail.com>
2019-03-28 13:25:34 -0600

    add 'return to main menu' and 'quit' functionality, began readme

--- bin/run.rb
+++ bin/run.rb
@@ -4,10 +4,26 @@ require 'colored'
 #binding.pry
 
 class CLI
+
+
+  def self.return_or_quit
+    puts "Type 'return' to return to the main menu. Type 'quit' to exit.".green.bold
+    response = gets.chomp
+    if response == "return"
+      self.main_menu
+    else
+      puts "Goodbye!".green.bold
+      self.quit
+    end
+  end
+
+  def self.quit
+    exit
+  end
+
+
   def self.main_menu
-    puts "Enter 'artists' to see upcoming shows they are in,\n
-          Enter 'venues' to see venues with upcoming shows,\n
-          Enter 'hottest show' to see the most popular artist and show location/information:\n".bold.green
+    puts "Enter 'artists' to see upcoming shows they are in,\nEnter 'venues' to see venues with upcoming shows,\nEnter 'hottest show' to see the most popular artist and show location/information:".bold.green
 
     @@response = gets.chomp
 
@@ -16,12 +32,14 @@ class CLI
       puts "\nEnter specific artist(s) to see the show information:".bold.green
       artist_prompt = gets.chomp
       Artist.get_one_artist(artist_prompt)
+      self.return_or_quit
 
     elsif @@response == "venues"
       Venue.get_all_venues
       puts "Enter venue to view specific show and artist:".bold.green
       venue_prompt = gets.chomp
       Venue.get_one_venue(venue_prompt)
+      self.return_or_quit
 
     elsif @@response == "hottest show"
       most_popular = Artist.all.map do |artist|
@@ -29,46 +47,14 @@ class CLI
       end
       most_popular = most_popular.sort!.reverse.first
       Artist.get_most_popular_artist(most_popular)
+      self.return_or_quit
 
     else
-      puts "Input not recognized. Please try artists, venues, shows"
+      puts "Input not recognized. Please try typing 'artists', 'venues', or 'shows'".red.bold
       self.main_menu
     end
-
   end
-
-
-
 
 
 self.main_menu
 end
-
-
-# puts "Enter artists to see upcoming shows they are in,\n
-#       Enter venues to see venues with upcoming shows,\n
-#       Enter hottest shows to see the most popular artist and show location/information:\n"
-# response = gets.chomp
-
-# if response == "artists"
-#   Artist.get_all_artists
-#   puts "\nEnter specific artist(s) to see the show information:"
-#   artist_prompt = gets.chomp
-#   Artist.get_one_artist(artist_prompt)
-#
-# elsif response == "venues"
-#   Venue.get_all_venues
-#   puts "Enter venue to view specific show and artist:"
-#   venue_prompt = gets.chomp
-#   Venue.get_one_venue(venue_prompt)
-#
-# elsif response == "hottest show"
-#   most_popular = Artist.all.map do |artist|
-#     artist["popularity"]
-#   end
-#   most_popular = most_popular.sort!.reverse.first
-#   Artist.get_most_popular_artist(most_popular)
-#
-# else
-#   puts "Input not recognized. Please try artists, venues, shows"
-# end
